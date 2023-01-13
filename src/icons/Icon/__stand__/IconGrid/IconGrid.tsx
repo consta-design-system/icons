a6cfdab8e27583b8beb2f09d7ec7b6d7dbe2ad03
gizeasy <gizeasy@gmail.com>
2023-01-13 14:28:18 +0300

    chore: edit config (#3)

--- src/icons/Icon/__stand__/IconGrid/IconGrid.tsx
+++ src/icons/Icon/__stand__/IconGrid/IconGrid.tsx
@@ -1,42 +1,30 @@
-import './IconGrid.css';
-
+import { Example } from '@consta/stand';
 import { Text } from '@consta/uikit/Text';
 import React from 'react';
 
-import { cn } from '##/utils/bem';
-
 import { groupedIcons } from '../../__mocks__/mock.data';
-import { IconPropSize, IconPropView } from '../../Icon';
-import { IconGridItem } from './IconGridItem';
 
-const cnIconGrid = cn('IconGrid');
+const getItemStatus = () => 'system' as const;
+const getItemDescription = (name: string) => name;
 
-type Props = {
-  size?: IconPropSize;
-  view?: IconPropView;
-};
-
-export function IconGrid(props: Props) {
-  const { size, view } = props;
-
+export function IconGrid() {
   return (
-    <div className={cnIconGrid()}>
+    <div>
       {groupedIcons.map(({ name, icons }) => (
-        <div className={cnIconGrid('Group')} key={cnIconGrid({ name })}>
+        <div key={name}>
           <Text size="xl" weight="semibold" view="primary">
             {name}
           </Text>
-          <div className={cnIconGrid('Container')}>
-            {Object.keys(icons).map((name) => (
-              <IconGridItem
-                key={name}
-                name={name}
-                icon={icons[name]}
-                size={size}
-                view={view}
-              />
-            ))}
-          </div>
+          <Example
+            col={{ 1: 0, 2: 500, 3: 700, 4: 890 }}
+            getItemStatus={getItemStatus}
+            getItemNode={(name) => {
+              const Icon = icons[name];
+              return <Icon />;
+            }}
+            getItemDescription={getItemDescription}
+            items={Object.keys(icons) as (keyof typeof icons)[]}
+          />
         </div>
       ))}
     </div>
