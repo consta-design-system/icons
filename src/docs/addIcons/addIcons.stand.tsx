1d41d9f125af53c9780b8dd6b73cda4558ac86f7
Maxim Kolesnikov <44449561+N1MBER@users.noreply.github.com>
2023-04-20 17:27:11 +0300

    docs(icons): add aliases (#10)
    consta-design-system/uikit#2955

--- src/docs/addIcons/addIcons.stand.tsx
+++ src/docs/addIcons/addIcons.stand.tsx
@@ -2,6 +2,7 @@ import { createStand } from '##/stand/standConfig';
 
 export default createStand({
   title: 'Создание иконок',
+  alias: ['иконки', 'создание', 'иконка'],
   id: 'addIcons',
   group: 'docs',
 });
