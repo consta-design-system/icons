f857b51147e82508f515c01e63dacba17d7c499c
Maxim Kolesnikov <44449561+N1MBER@users.noreply.github.com>
2023-08-30 19:16:00 +0300

    fix(Icons): fix sizes and position (#30)
    closes consta-design-system/uikit#2969

--- src/icons/IconArrowUp/IconArrowUp_size_m.tsx
+++ src/icons/IconArrowUp/IconArrowUp_size_m.tsx
@@ -3,7 +3,7 @@ import { SVGProps } from 'react';
 
 const IconArrowUpSizeM = (props: SVGProps<SVGSVGElement>) => (
   <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" {...props}>
-    <path d="m12 8-6 6 1.41 1.41L12 10.83l4.59 4.58L18 14l-6-6z" />
+    <path d="m12 8-6 6 1.41 1.41L12 10.83l4.59 4.58L18 14l-6-6Z" />
   </svg>
 );
 export default IconArrowUpSizeM;
