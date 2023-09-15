91aa8ef586ca04013792054cabd2c55ba30448cc
Maxim Kolesnikov <socialfox33@gmail.com>
2023-09-15 16:26:45 +0300

    fix(IconPlay, IconInfoCircle): fix svg paths

--- src/icons/IconPlay/IconPlay_size_m.tsx
+++ src/icons/IconPlay/IconPlay_size_m.tsx
@@ -3,7 +3,7 @@ import { SVGProps } from 'react';
 
 const IconPlaySizeM = (props: SVGProps<SVGSVGElement>) => (
   <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" {...props}>
-    <path d="M18 12 6 19V5l12 7z" />
+    <path d="M22.5 12L5.5 22L5.5 2L22.5 12Z" />
   </svg>
 );
 export default IconPlaySizeM;
--- src/icons/IconPlay/IconPlay_size_s.tsx
+++ src/icons/IconPlay/IconPlay_size_s.tsx
@@ -3,7 +3,7 @@ import { SVGProps } from 'react';
 
 const IconPlaySizeS = (props: SVGProps<SVGSVGElement>) => (
   <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" {...props}>
-    <path d="M13 8 3 14V2l10 6z" />
+    <path d="M15 8L3 15L3 1L15 8Z" />
   </svg>
 );
 export default IconPlaySizeS;
