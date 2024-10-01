08ce99b2ee3456c0d12e4549efb6691e65998946
gizeasy <gizeasy@gmail.com>
2024-10-01 17:01:21 +0300

    fix(IconSelect): update IconSelect and IconSelectOpen
    closes consta-design-system/uikit#3649

--- src/icons/IconSelectOpen/IconSelectOpen_size_s.tsx
+++ src/icons/IconSelectOpen/IconSelectOpen_size_s.tsx
@@ -3,7 +3,7 @@ import { SVGProps } from 'react';
 
 const IconSelectOpenSizeS = (props: SVGProps<SVGSVGElement>) => (
   <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" {...props}>
-    <path d="m8 5-4.5 5h9L8 5z" />
+    <path d="m8 5-4.5 5h9L8 5Z" />
   </svg>
 );
 export default IconSelectOpenSizeS;
