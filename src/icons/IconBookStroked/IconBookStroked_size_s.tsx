97e1b15e65d80ba7302e8b27ca730b5b27becae2
gizeasy <gizeasy@gmail.com>
2025-09-29 15:20:25 +0300

    feat(IconTranslate): add icon (#51)

--- src/icons/IconBookStroked/IconBookStroked_size_s.tsx
+++ src/icons/IconBookStroked/IconBookStroked_size_s.tsx
@@ -3,7 +3,11 @@ import { SVGProps } from 'react';
 
 const IconBookStrokedSizeS = (props: SVGProps<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" {...props}>
-    <path d="M10.646 15H5.354C4.055 15 3 13.86 3 12.45V2.91C3 1.86 3.79 1 4.764 1h6.472C12.21 1 13 1.86 13 2.91v7.95c0 .22-.036.44-.1.64h.1v1.27c0 .76-.31 1.41-.873 1.81-.4.28-.89.42-1.473.42h-.008ZM4.764 2.27c-.328 0-.591.29-.591.64v9.54c0 .7.527 1.27 1.172 1.27h5.291c.355 0 .628-.07.828-.21.227-.16.345-.41.345-.74H5.927a.638.638 0 0 1 0-1.27h5.291c.328 0 .591-.29.591-.64V2.91c0-.35-.263-.64-.59-.64H7.69V8l-1.173-.64L5.345 8V2.27h-.59.009Z" />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M12.102 2.005A1 1 0 0 1 13 3v11a1 1 0 0 1-.898.995L12 15H4a1 1 0 0 1-.995-.898L3 14V3a1 1 0 0 1 1-1h8l.102.005ZM4 14h8v-1H6a1 1 0 1 1 0-2h6V3H8v6l-1.5-.857L5 9V3H4v11Z"
+    />
   </svg>
 );
 export default IconBookStrokedSizeS;
